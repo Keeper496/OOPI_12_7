f49c5ffce2e02708ebd39de6df10fb33283492e8
karau <karau@DESKTOP-2KHKLHS>
2018-08-02 20:32:31 -0500

    Entered code for program.  Tested once and screen shot results.  Will continue cleaning up code over next few days.

--- src/group_project_12_7/Group_Project_12_7.java
+++ src/group_project_12_7/Group_Project_12_7.java
@@ -5,17 +5,84 @@
  */
 package group_project_12_7;
 
-/**
- *
- * @author keepe
- */
-public class Group_Project_12_7 {
+import java.util.Random;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+public class Group_Project_12_7
+{
+    // Create a method isBlurb() to check the string length
+    public static boolean isBlurb(String s)
+    {
+        try
+        {
+            int endOfWhoozit = prefixedByWhoozit(s);
+            if (endOfWhoozit >= s.length())
+                return false;
+            else
+                return isWhatszitList(s.substring(endOfWhoozit));
+        }
+        catch (StringIndexOutOfBoundsException ex)
+        {
+            return false;
+        }
     }
-    
+    // Create a method isWhatszitList() to the characters followed by q
+    public static boolean isWhatszitList(String s)
+    {
+        if ((!s.startsWith("qd") && !s.startsWith("qz")) || s.length() < 3)
+        {
+            return false;
+        }
+        s = s.substring(2);
+        int endOfWhoozit = prefixedByWhoozit(s);
+        if (endOfWhoozit == -1)
+            return false;
+        else if (endOfWhoozit == s.length())
+            return true;
+        else
+            return isWhatszitList(s.substring(endOfWhoozit));
+    }
+    // Create a prefixedByWhoozit() method to check the string is
+    // followed by Whoozit or not
+    public static int prefixedByWhoozit(String s)
+    {
+        int i = 0;
+        if (s.charAt(i) == 'x') {
+            i++;
+            while (i < s.length() && s.charAt(i) == 'y') {
+                i++;
+            }
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    // Create a main method
+    public static void main(String[] args)
+    {
+        // Create an object scan for the scanner class
+        Scanner scan = new Scanner(System.in);
+        String choice = "Y";
+        // Accept the input from the user Continuously
+        do
+        {
+            System.out.println("Enter text to check wether the input text is Blurb or not:");
+            String data = scan.nextLine();
+            // Call the isBlurb() method
+            boolean check = isBlurb(data);
+            if (check == true)
+            {
+                System.out.println("It is a Blurb");
+            }
+            else
+            {
+                System.out.println("It is not a Blurb");
+            }
+            System.out.print("Do you want to continue?(y/n): ");
+            choice = scan.nextLine();
+        } while (choice.equals("y") || choice.equals("Y"));
+    }
 }
